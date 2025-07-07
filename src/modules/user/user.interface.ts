52f8fb3901bb50d01cf2690a4e5a0278cfc1380f
Rubai Rahman <rubairahman1@gmail.com>
2025-07-07 17:01:07 +0600

    interface updated

--- src/modules/user/user.interface.ts
+++ src/modules/user/user.interface.ts
@@ -1,32 +1,14 @@
 import { Model } from 'mongoose';
 
-export type TUserName = {
-  firstName: string;
-  lastName: string;
-};
-export type TUserAddress = {
-  street: string;
-  city: string;
-  country: string;
-};
-
-export type TOrders = {
-  productName: string;
-  price: number;
-  quantity: number;
-};
-
 export type TUser = {
-  userId: number;
-  username: string;
-  password?: string | undefined;
-  fullName: TUserName;
-  age: number;
+  _id?: string;
+  authId?: string;
+  name: string;
   email: string;
+  role: string;
+  picture?: string;
   isActive: boolean;
-  hobbies: string[];
-  address: TUserAddress;
-  orders?: TOrders[];
+  emailVerified?: boolean;
 };
 
 export type UserMethods = {
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -4,5 +4,6 @@ import { UserController } from './user.controller';
 const router = express.Router();
 
 router.post('/', UserController.createUser);
+router.post('/sync', UserController.createUser);
 
 export const UserRoutes = router;
